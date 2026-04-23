704d4bedfe27484553f72b12a4a1e4b2c6a5d462
Yas5584 <ys136450@gmail.com>
2026-04-23 05:33:50 +0530

    Adding code

--- recommender/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import get_recommendations,get_movie_poster
-
-urlpatterns = [
-    path('recommend/', get_recommendations),
-    path('poster/', get_movie_poster)
-]
