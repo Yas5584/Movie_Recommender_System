3161b7a8ef9af526267b724174f18700552550e9
Yas5584 <139033335+Yas5584@users.noreply.github.com>
2026-04-27 23:29:49 +0530

    Implement data downloading and directory setup
    Refactored the recommender engine to include file downloading and data directory management.

--- recommender/engine.py
+++ recommender/engine.py
@@ -1,19 +1,47 @@
-import pandas as pd
-import numpy as np
-import pickle
-import os
-import requests
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-# movies_list=pickle.load(open('movies.pkl','rb'))
-print(BASE_DIR)
-movies_list=pickle.load(open(os.path.join(BASE_DIR, 'data/movies.pkl'), 'rb'))
+# import pandas as pd
+# import numpy as np
+# import pickle
+# import os
+# import requests
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# # movies_list=pickle.load(open('movies.pkl','rb'))
+# print(BASE_DIR)
+# movies_list=pickle.load(open(os.path.join(BASE_DIR, 'data/movies.pkl'), 'rb'))
 
-# print(movies_list['title'].values)
-similarity_vectors=pickle.load(open(os.path.join(BASE_DIR, 'data/similarity_vectors.pkl'), 'rb'))
+# # print(movies_list['title'].values)
+# similarity_vectors=pickle.load(open(os.path.join(BASE_DIR, 'data/similarity_vectors.pkl'), 'rb'))
 
 # print(similarity_vectors)
 
+import os
+import requests
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+DATA_DIR = os.path.join(BASE_DIR, 'data')
+
+os.makedirs(DATA_DIR, exist_ok=True)
+
+def download_file(url, path):
+    if not os.path.exists(path):
+        print("Downloading:", path)
+        r = requests.get(url)
+        with open(path, 'wb') as f:
+            f.write(r.content)
+
+
+MOVIES_URL = "https://drive.google.com/file/d/11qd3LPnzV8oGj2fALCDOQWcPmy-5oQKd/view?usp=drive_link"
+SIMILARITY_URL = "https://drive.google.com/file/d/1hCsoXM17X1fZJNKBMsbs3QNwcOUXgRAF/view?usp=drive_link"
+
+movies_path = os.path.join(DATA_DIR, 'movies.pkl')
+similarity_path = os.path.join(DATA_DIR, 'similarity_vectors.pkl')
+
+download_file(MOVIES_URL, movies_path)
+download_file(SIMILARITY_URL, similarity_path)
+
+import pickle
+
+movies_list = pickle.load(open(movies_path, 'rb'))
+ similarity_vectors = pickle.load(open(similarity_path, 'rb'))
 def recommend_movie(movie):
     list_movie=[]
     movie_index=movies_list[movies_list['title']==movie].index[0]
